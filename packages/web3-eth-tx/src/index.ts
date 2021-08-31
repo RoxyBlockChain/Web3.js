15a99cafe905c5c70f297e57197e5143d2488991
Wyatt Barnes <me@wyatt.email>
2021-08-30 15:15:27 -1000

    Make functions functional

--- packages/web3-eth-tx/src/index.ts
+++ packages/web3-eth-tx/src/index.ts
@@ -5,7 +5,7 @@ import {
     ValidTypesEnum,
     PrefixedHexString,
 } from 'web3-core-types/src/types';
-import { TransactionFactory } from '@ethereumjs/tx';
+import { TransactionFactory, TypedTransaction } from '@ethereumjs/tx';
 import { getBlockByNumber, getGasPrice } from 'web3-core-eth-method';
 import Web3CoreLogger from 'web3-core-logger';
 
@@ -23,7 +23,7 @@ const web3CoreLogger = new Web3CoreLogger(Web3EthTxErrorsConfig);
 export function initEthTx(
     txData: EthTxLegacy | EthTxEip2930 | EthTxEip1559,
     options: EthTxOptions
-) {
+): TypedTransaction {
     try {
         // Convert all ValidType properties to prefixed hex strings
         let modifiedTxData = formatOutputObject(
@@ -66,37 +66,41 @@ async function _fillProperties(
     txData: EthTxNormalized,
     web3Provider: IWeb3Provider
 ): Promise<EthTxNormalized> {
+    let _txData = {...txData}
+
     // Check if EIP-1559
     if (
-        txData.type === '0x2' ||
-        txData.maxPriorityFeePerGas !== undefined ||
-        txData.maxFeePerGas !== undefined
+        _txData.type === '0x2' ||
+        _txData.maxPriorityFeePerGas !== undefined ||
+        _txData.maxFeePerGas !== undefined
     ) {
-        txData = await _fillEip1559(txData, web3Provider);
+        _txData = await _fillEip1559(_txData, web3Provider);
     } else {
-        if (txData.accessList) txData.type = '0x1';
+        if (_txData.accessList) _txData.type = '0x1';
         const gasPrice = await getGasPrice(web3Provider);
-        txData.gasPrice = gasPrice.result as PrefixedHexString;
+        _txData.gasPrice = gasPrice.result as PrefixedHexString;
     }
 
-    return txData;
+    return _txData;
 }
 
 async function _fillEip1559(
     txData: EthTxNormalized,
     web3Provider: IWeb3Provider
 ): Promise<EthTxNormalized> {
-    txData.type = '0x2';
-    if (txData.maxPriorityFeePerGas === undefined)
-        txData.maxPriorityFeePerGas = '0x3B9ACA00'; // 1 Gwei
+    let _txData = txData
 
-    if (txData.maxFeePerGas === undefined) {
+    _txData.type = '0x2';
+    if (_txData.maxPriorityFeePerGas === undefined)
+        _txData.maxPriorityFeePerGas = '0x3B9ACA00'; // 1 Gwei
+
+    if (_txData.maxFeePerGas === undefined) {
         if (web3Provider === undefined)
             throw web3CoreLogger.makeError(
                 Web3EthTxErrorNames.invalidTxEip1559,
                 {
                     reason: 'Missing maxFeePerGas property, and no web3Provider available to fetch value',
-                    params: { txData },
+                    params: { txData: _txData },
                 }
             );
 
@@ -106,12 +110,12 @@ async function _fillEip1559(
             false
         );
         if (block.result?.baseFeePerGas) {
-            txData.maxFeePerGas = (
+            _txData.maxFeePerGas = (
                 BigInt(block.result.baseFeePerGas) * BigInt(2) +
-                BigInt(txData.maxPriorityFeePerGas)
+                BigInt(_txData.maxPriorityFeePerGas)
             ).toString(16);
         }
     }
 
-    return txData;
+    return _txData;
 }
